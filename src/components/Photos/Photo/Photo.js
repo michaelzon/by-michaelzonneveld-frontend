21b481edfa73ddf23fae65f1ce53e21fa20a3b0d
Michael Zonneveld <michael.zonneveld@live.nl>
2024-03-23 10:01:39 +0100

    commented photo section

--- src/components/Photos/Photo/Photo.js
+++ src/components/Photos/Photo/Photo.js
@@ -1,42 +1,43 @@
-import React, {useEffect, useState} from 'react';
-import {useParams} from 'react-router-dom';
-import './photo.css';
+// import React, {useEffect, useState} from 'react';
+// import {useParams} from 'react-router-dom';
+// import Comments from "../Comments/Comments";
+// import './photo.css';
 
 
-export default function Photo({photos}) {
-    const [photo, setPhoto] = useState(null);
-    const [error, setError] = useState(null);
-    const photoId = parseInt(useParams().id)
+// export default function Photo({photos}) {
+//     const [photo, setPhoto] = useState(null);
+//     const [error, setError] = useState(null);
+//     const photoId = parseInt(useParams().id)
 
-    useEffect(() => {
-        const retrievedPhoto = photos.find(p => p.id === photoId);
-        if (retrievedPhoto) {
-            setPhoto(retrievedPhoto);
-        } else {
-            setError('Photo not found');
-        }
-    }, [photos, photoId])
+//     useEffect(() => {
+//         const retrievedPhoto = photos.find(p => p.id === photoId);
+//         if (retrievedPhoto) {
+//             setPhoto(retrievedPhoto);
+//         } else {
+//             setError('Photo not found');
+//         }
+//     }, [photos, photoId])
 
-    if (error) {
-        return <div className={"error-message"}>Error: {error}</div>
-    }
+//     if (error) {
+//         return <div className={"error-message"}>Error: {error}</div>
+//     }
 
-    return (
-        <div className={'photo__flex-box--column'}>
-            {photo ?
-                <div className={'photo__flex-box--row'}>
-                    <div className={'photo__image'}>
-                        <img src={photo.url} alt={photo.title}/>
-                    </div>
-                    <div className={'photo__description'}>
-                        <span> {photo.location}</span>
-                        <span> {photo.date}</span>
-                    </div>
-                </div>
-                :
-                <p> Loading photo... </p>
-
-            }
-        </div>
-    )
-}
+//     return (
+//         <div className={'photo__flex-box--column'}>
+//             {photo ?
+//                 <div className={'photo__flex-box--row'}>
+//                     <div className={'photo__image'}>
+//                         <img src={photo.url} alt={photo.title}/>
+//                     </div>
+//                     <div className={'photo__description'}>
+//                         <span> {photo.location}</span>
+//                         <span> {photo.date}</span>
+//                     </div>
+//                 </div>
+//                 :
+//                 <p> Loading photo... </p>
+//             }
+//             <Comments></Comments>
+//         </div>
+//     )
+// }
--- src/components/Photos/PhotoGallery/PhotoGallery.js
+++ src/components/Photos/PhotoGallery/PhotoGallery.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import './photo-gallery.css';
-import {Link, useParams} from "react-router-dom";
+// import React from 'react';
+// import './photo-gallery.css';
+// import {Link, useParams} from "react-router-dom";
 
-export default function PhotoGallery({photos}) {
-    const { album } = useParams();
-    const photosFromAlbum = photos.filter(photo => photo.album === album);
+// export default function PhotoGallery({photos}) {
+//     const { album } = useParams();
+//     const photosFromAlbum = photos.filter(photo => photo.album === album);
 
-    return (
-        <div className='container__items'>
-            {photosFromAlbum.map(photo => (
-                <div key={photo.id} className='item'>
-                    <Link to={`/${photo.album}/${photo.id}`}>
-                        <img src={photo.url} alt={photo.title}/>
-                    </Link>
-                </div>
-            ))}
-        </div>
-    );
-};
+//     return (
+//         <div className='container__items'>
+//             {photosFromAlbum.map(photo => (
+//                 <div key={photo.id} className='item'>
+//                     <Link to={`/${photo.album}/${photo.id}`}>
+//                         <img src={photo.url} alt={photo.title}/>
+//                     </Link>
+//                 </div>
+//             ))}
+//         </div>
+//     );
+// };
